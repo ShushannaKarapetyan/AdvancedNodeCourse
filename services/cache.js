bac1c59c9b432be04951a30f006cd9b85d0ae413
Shushanna <shushanna.karapetyan.97@gmail.com>
2023-04-23 22:31:54 +0400

    Clearing nested hashes, automated cache clearing with middleware

--- services/cache.js
+++ services/cache.js
@@ -4,12 +4,20 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
-
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+
+    return this;
+}
+
 mongoose.Query.prototype.exec = async function () {
-    console.log("I'm about to run a query");
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
 
     // See if we have a value for 'key' in redis
     const key = JSON.stringify(
@@ -18,20 +26,28 @@ mongoose.Query.prototype.exec = async function () {
         })
     );
 
-    const cacheValue = await client.get(key)
+    const cacheValue = await client.hget(this.hashKey, key)
 
     // if we do, return that
     if (cacheValue) {
-        // get an instance of the model
-        const doc = new this.model(JSON.parse(cacheValue));
+        const doc = JSON.parse(cacheValue);
 
-        return doc;
+        // check if doc is array or not and return an instance of the model
+        return Array.isArray(doc)
+            ? doc.map(d => new this.model(d))
+            : new this.model(doc)
     }
 
     // otherwise, issue the query and store the result in redis
     const result = await exec.apply(this, arguments);
 
-    client.set(key, JSON.stringify(result));
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
     return result;
+}
+
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    }
 }
