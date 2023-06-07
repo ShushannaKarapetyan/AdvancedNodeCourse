70632beaa67cdacc06d56eaba1e79f864643ae51
Shushanna <shushanna.karapetyan.97@gmail.com>
2023-06-08 01:05:18 +0400

    Tested oauth flow

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID_DEV,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET_DEV,
     mongoURI: process.env.MONGO_URI_DEV,
-    cookieKey: '',
+    cookieKey: process.env.COOKIE_KEY,
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_BUCKET_REGION,
--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer');
 
 let browser, page;
@@ -12,8 +13,50 @@ beforeEach(async () => {
     await page.goto('localhost:3000');
 });
 
-test('We can lunch a browser', async () => {
+afterEach(async () => {
+    await browser.close();
+})
+
+test('the header has the correct text', async () => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
     expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+    await page.click('.right a');
+
+    const url = page.url();
+
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('when signed in, shows logout button', async () => {
+    const id = '644505ed6bf6ad2a405ddc3b';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    };
+
+    const sessionString = Buffer.from(
+        JSON.stringify(sessionObject)
+    ).toString('base64');
+
+    const Keygrip = require('keygrip');
+    const keys = require("../../config/keys");
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    await page.setCookie({name: 'session', value: sessionString});
+    await page.setCookie({name: 'session.sig', value: sig});
+    await page.goto('localhost:3000');
+
+    const element = 'a[href="/auth/logout"]';
+    await page.waitFor(element);
+    const text = await page.$eval(element, el => el.innerHTML);
+
+    expect(text).toEqual('Logout');
 });
