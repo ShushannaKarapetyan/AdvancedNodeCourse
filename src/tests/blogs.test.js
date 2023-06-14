d954c4520b4cd2d24cdcd6bed400dd416ad28266
Shushanna <shushanna.karapetyan.97@gmail.com>
2023-06-14 16:04:38 +0400

    Testing blog creation using invalid inputs

--- src/tests/blogs.test.js
+++ src/tests/blogs.test.js
@@ -12,11 +12,31 @@ afterEach(async () => {
     await page.close();
 });
 
-test('When logged in, can see blog create form', async () => {
-    await page.login();
-    await page.click('a.btn-floating');
+describe('When logged in', async () => {
+    beforeEach(async () => {
+        await page.login();
+        await page.click('a.btn-floating');
+    });
 
-    const label = await page.getContentsOf('form label');
 
-    expect(label).toEqual('Blog Title');
+    test('can see blog create form', async () => {
+        const label = await page.getContentsOf('form label');
+
+        expect(label).toEqual('Blog Title');
+    });
+
+    describe('And using invalid inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+
+
+        test('the form shows an error message', async () => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const contentError = await page.getContentsOf('.content .red-text');
+
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        });
+    });
 });
